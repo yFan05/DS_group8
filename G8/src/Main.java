4d6afc481a7d62024e8f874a62652a2334285a01
tzu0713 <147122879+tzu0713@users.noreply.github.com>
2024-12-16 22:44:48 +0800

    Update Main.java

--- G8/src/Main.java
+++ G8/src/Main.java
@@ -1,1 +1,6 @@
-
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
