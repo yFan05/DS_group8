793bd7d2a6c5bfe23019698fca2d6ea67dde440e
tzu0713 <147122879+tzu0713@users.noreply.github.com>
2024-12-16 22:45:43 +0800

    Update Main.java

--- G8/src/Main.java
+++ G8/src/Main.java
@@ -4,3 +4,7 @@ import java.util.Scanner;
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLSession;
+
+public class Main
+{
+  public static void main(String[] args) throws IOException {
