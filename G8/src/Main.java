ed5b7dd9dc1a78e7f4313249b6756eaa14d97db9
郭孟綺 <147123477+sophia112306@users.noreply.github.com>
2024-12-16 22:54:18 +0800

    Update Main.java

--- G8/src/Main.java
+++ G8/src/Main.java
@@ -8,3 +8,12 @@ import javax.net.ssl.SSLSession;
 public class Main
 {
   public static void main(String[] args) throws IOException {
+
+    Keyword k1=new Keyword(綠色,3);
+    Keyword k2=new Keyword(綠能,3);
+     Keyword k3=new Keyword(能源,3);
+     Keyword k4=new Keyword(氣候,3);
+     Keyword k5=new Keyword(環保,3);
+     Keyword k6=new Keyword(環境,3);
+     Keyword k7=new Keyword(政策,2);
+     Keyword k8=new Keyword(法規,2);
