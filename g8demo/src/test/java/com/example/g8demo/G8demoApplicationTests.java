a5fa9abfe1b0d8652fc4dc0584b4ccffa421869e
112306089 <112306089@g.nccu.edu.tw>
2024-12-18 15:18:19 +0800

    1218-1

---
+++ g8demo/src/test/java/com/example/g8demo/G8demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.g8demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G8demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
