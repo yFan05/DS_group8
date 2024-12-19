acf6a2a53161df5798cc1c247ecfac8d638d528d
112306089 <147122994+112306089@users.noreply.github.com>
2024-12-19 14:21:57 +0800

    Update WordCounter.java

--- g8demo/src/main/java/google/demo/model/WordCounter.java
+++ g8demo/src/main/java/google/demo/model/WordCounter.java
@@ -7,59 +7,82 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 
 public class WordCounter
 {
-	private String urlStr;
-	private String content;
+    private String urlStr;
+    private String content;
+    private String title;
 
-	public WordCounter(String urlStr)
-	{
-		this.urlStr = urlStr;
-	}
+    public WordCounter(String urlStr)
+    {
+        this.urlStr = urlStr;
+    }
 
-	private String fetchContent() throws IOException
-	{
-		
-		URL url = new URL(this.urlStr);
-		URLConnection conn = url.openConnection();
-		InputStream in = conn.getInputStream();
-		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+    private String fetchContent() throws IOException
+    {
+        URL url = new URL(this.urlStr);
+        URLConnection conn = url.openConnection();
+        InputStream in = conn.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
 
-		String retVal = "";
+        String retVal = "";
 
-		String line = null;
+        String line = null;
 
-		while ((line = br.readLine()) != null)
-		{
-			retVal = retVal + line + "\n";
-		}
+        while ((line = br.readLine()) != null)
+        {
+            retVal = retVal + line + "\n";
+        }
 
-		return retVal;
-	}
+        return retVal;
+    }
 
-	public int countKeyword(String keyword) throws IOException
-	{
-		if (content == null)
-		{
-			content = fetchContent();
-		}
+    public int countKeyword(String keyword) throws IOException
+    {
+        if (content == null)
+        {
+            content = fetchContent();
+        }
 
-		// To do a case-insensitive search, we turn the whole content and keyword into
-		// upper-case:
-		content = content.toUpperCase();
-		keyword = keyword.toUpperCase();
+        // To do a case-insensitive search, we turn the whole content and keyword into
+        // upper-case:
+        content = content.toUpperCase();
+        keyword = keyword.toUpperCase();
 
-		int retVal = 0;
-		int fromIdx = 0;
-		int found = -1;
+        int retVal = 0;
+        int fromIdx = 0;
+        int found = -1;
 
-		while ((found = content.indexOf(keyword, fromIdx)) != -1)
-		{
-			retVal++;
-			fromIdx = found + keyword.length();
-		}
+        while ((found = content.indexOf(keyword, fromIdx)) != -1)
+        {
+            retVal++;
+            fromIdx = found + keyword.length();
+        }
 
-		return retVal;
-	}
+        return retVal;
+    }
+
+    // 計算標題中關鍵字的次數
+    public int countKeywordInTitle(ArrayList<Keyword> keywords) throws IOException {
+        if (content == null) {
+            content = fetchContent();
+        }
+
+        // 提取標題
+        if (title == null) {
+            title = content.split("<title>")[1].split("</title>")[0];
+        }
+
+        int count = 0;
+        for (Keyword k : keywords) {
+            int index = title.toUpperCase().indexOf(k.name.toUpperCase());
+            if (index != -1) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
