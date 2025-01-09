bba1edc26be23432fc5f8371002c598db3af73c2
郭孟綺 <147123477+sophia112306@users.noreply.github.com>
2025-01-09 19:51:16 +0800

    Update GoogleSearchController.java

--- g8demo/src/main/java/google/demo/controller/GoogleSearchController.java
+++ g8demo/src/main/java/google/demo/controller/GoogleSearchController.java
@@ -1,42 +1,28 @@
 package google.demo.controller;
 
 import google.demo.service.GoogleQueryService;
-import google.demo.model.Keyword;
-import google.demo.model.WebPage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:8080") // 根據你的前端地址調整
 public class GoogleSearchController {
 
-    @Autowired
-    private GoogleQueryService googleQueryService;
+	@Autowired
+	private GoogleQueryService googleQueryService;
 
-    @GetMapping("/search")
-    public HashMap<String, String> search(@RequestParam("q") String query) {
-        try {
-            ArrayList<Keyword> keywords = new ArrayList<>();
-            keywords.add(new Keyword("綠色", 8));
-            keywords.add(new Keyword("能源", 6));
-            keywords.add(new Keyword("綠能", 10));
-            keywords.add(new Keyword("環保", 10));
-            
-            ArrayList<WebPage> webPages = googleQueryService.searchAndCalculateScore(query, keywords);
+	@GetMapping("/search")
+	public HashMap<String, String> search(@RequestParam("q") String query) {
 
-            HashMap<String, String> results = new HashMap<>();
-            for (WebPage page : webPages) {
-                results.put(page.getTitle(), page.getUrl());
-            }
-            return results;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return new HashMap<>();
-        }
-    }
+		try {
+			return googleQueryService.search(query);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return new HashMap<>();
+		}
+	}
 }
