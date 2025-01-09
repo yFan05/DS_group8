eb21b6bfc568a8281061cef0812d6f803bb4ba1d
112306089 <147122994+112306089@users.noreply.github.com>
2025-01-09 18:22:47 +0800

    Update 同義詞 GoogleSearchController.java

--- g8demo/src/main/java/google/demo/controller/GoogleSearchController.java
+++ g8demo/src/main/java/google/demo/controller/GoogleSearchController.java
@@ -1,28 +1,40 @@
 package google.demo.controller;
 
 import google.demo.service.GoogleQueryService;
+import google.demo.model.Keyword;
+import google.demo.model.WebPage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:8080") // 根據你的前端地址調整
+@CrossOrigin(origins = "http://localhost:8080")
 public class GoogleSearchController {
 
-	@Autowired
-	private GoogleQueryService googleQueryService;
+    @Autowired
+    private GoogleQueryService googleQueryService;
 
-	@GetMapping("/search")
-	public HashMap<String, String> search(@RequestParam("q") String query) {
+    @GetMapping("/search")
+    public HashMap<String, String> search(@RequestParam("q") String query) {
+        try {
+            ArrayList<Keyword> keywords = new ArrayList<>();
+            keywords.add(new Keyword("綠色", 1.0));
+            keywords.add(new Keyword("能源", 0.9));
+            
+            ArrayList<WebPage> webPages = googleQueryService.searchAndCalculateScore(query, keywords);
 
-		try {
-			return googleQueryService.search(query);
-		} catch (IOException e) {
-			e.printStackTrace();
-			return new HashMap<>();
-		}
-	}
+            HashMap<String, String> results = new HashMap<>();
+            for (WebPage page : webPages) {
+                results.put(page.getTitle(), page.getUrl());
+            }
+            return results;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new HashMap<>();
+        }
+    }
 }
