79f63b7687598d8eb5fcdf2acf6a2be96a1b23cf
112306089 <147122994+112306089@users.noreply.github.com>
2025-01-09 17:32:40 +0800

    Update GoogleSearchController.java

--- g8demo/src/main/java/google/demo/controller/GoogleSearchController.java
+++ g8demo/src/main/java/google/demo/controller/GoogleSearchController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.HashMap.ojf;
 
 @RestController
 @RequestMapping("/api")
